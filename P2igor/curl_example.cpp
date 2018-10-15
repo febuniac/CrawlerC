3ff05f92770bbf2c048de7a228d4ad8b5eb938c8
Felipe Buniac <febuniac@gmail.com>
2018-10-15 10:12:06 -0300

    bug do amp resolvido

--- P2igor/curl_example.cpp
+++ P2igor/curl_example.cpp
@@ -9,6 +9,7 @@ using namespace std;
 #include <iterator>
 std::vector< string > lista_links_produtos;
 std::vector< string >lista_links_paginas;
+
 // Convert curl output into a string
 static size_t WriteCallback(void *contents, size_t size, size_t nmemb, void *userp)
 {
@@ -16,7 +17,7 @@ static size_t WriteCallback(void *contents, size_t size, size_t nmemb, void *use
     return size * nmemb;
 }
 
-// reading the file with program info
+// writing a file with program info (HTML page)
 void write_file(string info){
     ofstream myfile;
     myfile.open ("mycurlfile.txt");
@@ -24,7 +25,7 @@ void write_file(string info){
     myfile.close();
 }
 
-
+//receives as input an URL and makes the download of the HTMl code of the page
 string curl_downloadHTML(std::string url){
     CURL *curl; //new curl handle to restart the connection
     CURLcode res;
@@ -45,6 +46,7 @@ string curl_downloadHTML(std::string url){
     return readBuffer;
 }
 
+//Parse tyhe HTML page looking for products and append the products link into a list of product links
 void regex_parseHTML_prods(std::string html_pag){
     std::regex linksprod_reg("<a class=\"card-product-url\" href=\"([^\"]+)\"");
     //auto html_pag= curl_downloadHTML(url); //My string in HTML whole page (reasBuffer)
@@ -70,6 +72,7 @@ void regex_parseHTML_prods(std::string html_pag){
     }
 }
 
+//Parse tyhe HTML page looking for the next button page and append the next page link  into a list of next page  links and return the next page of the actual page
 std::string regex_parseHTML_next_page(std::string html_pag){
     CURL *curl; 
     std::regex linkspages_reg("<li class=\"\"><a href=\"([^<]+)\"><span aria-label=\"Next\">");
@@ -87,6 +90,11 @@ std::string regex_parseHTML_next_page(std::string html_pag){
         std::smatch match = *i;  
         std::string match_str_next = match[1].str();
         link_com_site_antes_n = "www.submarino.com.br" + match_str_next;
+        std::regex amp("amp;");
+        // std::regex_replace(std::ostreambuf_iterator<char>(std::cout),
+        //               link_com_site_antes_n.begin(), link_com_site_antes_n.end(), amp, "");
+        link_com_site_antes_n = std::regex_replace(link_com_site_antes_n, amp, "");
+        std::cout<<link_com_site_antes_n<< "lok"<<'\n';
         lista_links_paginas.push_back(link_com_site_antes_n);
         //curl_downloadHTML(lista_links_paginas[i]);
         //std::cout << match_str_next << '\n';
@@ -97,14 +105,14 @@ std::string regex_parseHTML_next_page(std::string html_pag){
     return link_com_site_antes_n;//return link da pagina de next
 }
 
+//Does a whole cycle of wdownloading products and going to the next page
 std::vector<string> regex_parseHTML_next_page_loop(std::string url){
-    std::vector< string > next_link = lista_links_paginas;
-    std::cout <<"OIE"<<lista_links_paginas.size() << '\n';
+    // std::vector< string > next_link = lista_links_paginas;
     // std::string no_next_link = regex_parseHTML_no_next_page();
     std::string vazio ="";
-    for (int i = 0; i <= lista_links_paginas.size(); ++i){
+    for (int i = 0; i <=lista_links_paginas.size(); ++i){
         std::cout <<"Página Next:"<<i<< lista_links_paginas[i] << '\n';
-        while(next_link[i] != vazio){
+        while(lista_links_paginas[i] != vazio){
             std::string html_page = curl_downloadHTML(url);
             regex_parseHTML_prods(html_page);
             url = regex_parseHTML_next_page(html_page);
@@ -132,8 +140,25 @@ void regex_download_prod_page_loop(){
 
 int main(void)
 {
-    regex_parseHTML_next_page_loop("https://www.submarino.com.br/categoria/bebes/brinquedos-para-bebe");
+    //regex_parseHTML_next_page_loop("https://www.submarino.com.br/categoria/bebes/brinquedos-para-bebe");
+    std::string html_page = curl_downloadHTML("https://www.submarino.com.br/categoria/bebes/brinquedos-para-bebe");
+    regex_parseHTML_prods(html_page);
+    std::string url = regex_parseHTML_next_page(html_page);
+    // html_page = curl_downloadHTML(url);
+    // regex_parseHTML_prods(html_page);
+    // std::cout<<url<<"\n";
+    // url = regex_parseHTML_next_page(html_page);
+    // html_page = curl_downloadHTML(url);
+    // std::cout<<"cade"<<url<<"\n";
     //regex_parseHTML_next_page("https://www.submarino.com.br/categoria/bebes/brinquedos-para-bebe");
+    // for (int i = 0; i <= lista_links_paginas.size(); ++i){
+    //     std::cout <<"Página Next:"<<i<< lista_links_paginas[i] << '\n';
+    //     while(lista_links_paginas[i] != ""){
+    //         std::string html_page = curl_downloadHTML("https://www.submarino.com.br/categoria/bebes/brinquedos-para-bebe");
+    //         regex_parseHTML_prods(html_page);
+    //         std::string url = regex_parseHTML_next_page(html_page);
+    //     }
+    // }
     return 0;
 }
 
